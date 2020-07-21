1f1b4f9bb0f643b39f73d7c051f914d1bc022aea
parisgirl16 <40363354+parisgirl16@users.noreply.github.com>
2020-07-21 16:26:28 -0500

    Add files via upload

---
+++ journal.js
@@ -0,0 +1,10 @@
+// var journalData = {
+//   journalTitle: document.getElementById("journalTitle").innerHTML,
+//   journalDate: document.getElementById("journalDate").innerHTML,
+//   journalEntry: document.getElementById("journalEntry").innerHTML,
+//   submitButton: document.getElementById("submitButton").innerHTML
+// };
+//
+// function submitButtonClicked() {
+//   $(".newJournals").append('<button type="button" value="`${journalData.journalDate} ${journalData.journalTitle}`"></button>');
+// }
